8e002015865a7280a79a30d6ce33efb5eeecc54c
V3LLUM <pontipvong@gmail.com>
2026-04-19 21:39:12 -0400

    Create v3llum-title.script.js

---
+++ v3llum-title.script.js
@@ -0,0 +1,11 @@
+<script>
+<!-- //SCRIPT - NOTIFICATION POP-UP //START -->
+$("document").on("click", "body", function () {
+	$(".notify").toggleClass("animate");
+});
+<!-- //SCRIPT - NOTIFICATION POP-UP //END -->
+
+</script>
+
+
+
